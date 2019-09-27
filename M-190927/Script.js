dfa906845f8abf2a954a2f6c2c14772f01a9a5ab
mendritzk.io <hi@mendritzk.io>
2019-09-27 10:57:59 +0200

    Styling added + challenge 1 finished

--- M-190927/Script.js
+++ M-190927/Script.js
@@ -4,7 +4,11 @@
 ///////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
+
+
+////////////////////////////////////////////////////////////////////////
 // EXCERCISE 1/////////////////////////////////
+/////////////////////////////////////////777///////seven///////////7////
 let cty1 = {name: "Aburg", population: 800000, continent: "Arope", advice:"positive"}
 let cty2 = {name: "Bburg", population: 1800000, continent: "Brope", advice:"negative"}
 let cty3 = {name: "Cburg", population: 2800000, continent: "Crope", advice:"positive"}
@@ -17,17 +21,33 @@ let ctyS = [cty1,cty2,cty3,cty4]
 function chckAdviceCity (info) {
 return `${info.name} has a population of ${info.population} and is located in ${info.continent}.`
 }
-
+document.write("<br><br>//EXCERCISE 1 /////////////////////////////////<br>");
 document.write(chckAdviceCity(cty1))
 
-document.write("<br><br>");
 
+
+
+
+////////////////////////////////////////////////////////////////////////
 //CHALLENGE 1 /////////////////////////////////
-
+////////////////////////////////////////////////////////////////////////
 function chckCity (arrOfCities){
-    let advc = Math.min.apply(Math,arrOfCities[i].name)
-
-    return `You definitely should go to ${arrOfCities[i].name}. It is the smalles city in our Portfolio.`
+    let temp = arrOfCities[0];
+    for (i=0; i<arrOfCities.length; i++){
+        if (arrOfCities[i].population<temp.population ){
+            temp = arrOfCities[i]
+        }
+    }
+    // return temp;
+    return `You definitely should go to ${temp.name}. It is the smalles city in our Portfolio.`
 }
 
+document.write("<br><br>//CHALLENGE 1 /////////////////////////////////<br>");
 document.write(chckCity(ctyS))
+
+////////////////////////////////////////////////////////////////////////
+//CHALLENGE 2 /////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+
+document.write("<br><br>//CHALLENGE 2 /////////////////////////////////<br>");
