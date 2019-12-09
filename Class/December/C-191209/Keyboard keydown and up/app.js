7280dfcf0679dde25d1ec0de8df0bb47a23afd62
mendritzk.io <hi@mendritzk.io>
2019-12-09 16:04:23 +0100

    Day Done

--- Class/December/C-191209/Keyboard keydown and up/app.js
+++ Class/December/C-191209/Keyboard keydown and up/app.js
@@ -6,18 +6,22 @@ let button = document.querySelector("#clear");
 
 input.onkeydown = displayInfo;
 input.onkeyup = displayInfo;
-input.onclick = (e)=> {e.target.value = "";}
-button.onclick = clearText;
+input.onclick = (e)=> {e.target.value = "";};
+input.onblur = (e)=> {e.target.value = "Press keys here"};
+button.onclick = ()=> {textarea.value = "";input.value = "";textarea.value = "";input.focus();}
 
 function displayInfo(e){
     //e.target.value = ""
-    textarea.value += `${e.type} key=${e.key} code=${e.code}
-    `
+    if(e.type== "keydown"){
+        if (e.key==="Shift"||"Control"||"Alt"){
+            textarea.value += `${e.type} key=${e.key} code=${e.code} ${e.key}key\n`
+        }
+        else { textarea.value += `${e.type} key=${e.key} code=${e.code}\n`  
+    }}
+    
+    else if(e.type=="keyup"){
+    textarea.value += `${e.type} key=${e.key} code=${e.code}\n ${new Array(50).join("-")}\n`
+    }
 }
 
-function clearText(e){
-textarea.value = "";
-input.value = "";
-input.focus();
-}
 
