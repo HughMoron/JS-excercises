340a6ba4ff7f3dbbfd8a76dee5db7f25af61a46a
mendritzk.io <hi@mendritzk.io>
2019-12-10 14:24:17 +0100

    Event Bubbling added

--- Class/December/C-191209/Keyboard keydown and up/app.js
+++ Class/December/C-191209/Keyboard keydown and up/app.js
@@ -2,9 +2,10 @@
 let input = document.querySelector("#textinput");
 let textarea = document.querySelector("textarea");
 let button = document.querySelector("#clear");
-let keydownPrevent = document.querySelector("#keydown")
-let keydownIgnore = document.querySelector("#keydown2")
-let keyupIgnore = document.querySelector("#keyup2")
+let keydownPrevent = document.querySelector("#keydown");
+let keydownIgnore = document.querySelector("#keydown2");
+let keyupIgnore = document.querySelector("#keyup2");
+let lastTime = Date.now();
 
 //ONE-LINE-FUNCTIONS
 input.onkeydown = displayInfo;
@@ -24,19 +25,25 @@ button.onclick = () => {
 
 //EVENTHANDLER FUNCTION
 function displayInfo(e) {
-    if (keydownIgnore.checked || keyupIgnore.checked) return
+    if (keydownIgnore.checked || keyupIgnore.checked) {return};
+    
     if (keydownPrevent.checked) {
         e.preventDefault()
     }
     if (e.type == "keydown") {
+        
         if (e.key !== "Shift" || "Control" || "Alt") {
             textarea.value += `${e.type} key=${e.key} code=${e.code} \n`
-        }      
+        } 
+        
         else {
             textarea.value += `${e.type} key=${e.key} code=${e.code} ${e.key}key\n`
         } 
     } 
     else if (e.type == "keyup") {
-        textarea.value += `${e.type} key=${e.key} code=${e.code}\n ${new Array(50).join("-")}\n`
+        textarea.value += `${e.type} key=${e.key} code=${e.code}\n` 
+    }
+    if(e.type == "keyup" && Date.now()-lastTime >250){
+        textarea.value += new Array(50).join("-")+"\n"
     }
 }
