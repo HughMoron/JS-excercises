1c76610eea994e068151816ea2db0b6184db2793
mendritzk.io <hi@mendritzk.io>
2019-12-16 14:41:07 +0100

    Task List Challenges

---
+++ Class/December/04.taskList/app.js
@@ -0,0 +1,67 @@
+// Define UI Variables
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+//CONSOLE.LOG
+
+
+//LOAD ALL EVENT LISTENERS
+loadEventListeners();
+//DEFINING THE EVENT LISTENERS
+function loadEventListeners() {
+    //ADD TASK EVENT
+    form.addEventListener('submit', addTask)
+    //REMOVE TASK EVENTS
+    taskList.addEventListener("click", removeTask)
+    //CLEAR TASKS EVENT
+    clearBtn.addEventListener("click", removeTask)
+}
+
+//ADD TASK EVENT HANDLER
+function addTask(e) {
+    if (!taskInput.value) {
+        alert("Add a task")
+    } else {
+        let li = document.createElement("li");
+
+        li.className = "collection-item"
+
+        let textNode = document.createTextNode(taskInput.value);
+
+        li.appendChild(textNode);
+
+        let aNew = document.createElement("a");
+
+        aNew.className = "delete-item secondary-content";
+
+        aNew.innerHTML = "<i class='fa fa-remove'></i>";
+
+        li.appendChild(aNew);
+
+        taskList.appendChild(li)
+
+        taskInput.blur();
+
+        taskInput.value = "";
+
+        document.querySelector("label").classList.remove("active");
+
+        e.preventDefault();
+    }
+}
+
+
+//REMOVE TASK EVENT
+function removeTask(e) {
+    if (e.target.closest("li")) {
+        e.target.closest("li").remove()
+    }
+    if (e.target === clearBtn) {
+        Array.from(document.querySelectorAll("li")).forEach(element => {
+            element.remove();
+        });
+    }
+}
