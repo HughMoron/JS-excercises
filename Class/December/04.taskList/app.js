709652aa9bb0ac8f93293ceee7e244f44241be90
mendritzk.io <hi@mendritzk.io>
2019-12-18 10:23:27 +0100

    I gave up.

--- Class/December/04.taskList/app.js
+++ Class/December/04.taskList/app.js
@@ -19,7 +19,7 @@ function loadEventListeners() {
     //CLEAR TASKS EVENT
     clearBtn.addEventListener("click", removeTask)
     // FILTER THE TASK
-    filter.addEventListener("keyup",filterTasks)
+    filter.addEventListener("keyup", filterTasks)
 
 }
 
@@ -59,23 +59,58 @@ function addTask(e) {
 //REMOVE TASK EVENT
 function removeTask(e) {
     if (e.target.closest("li")) {
-        if(confirm("Are you sure?")){
-        e.target.closest("li").remove()}
+        if (confirm("Are you sure?")) {
+            e.target.closest("li").remove();
+            tasks.splice(i,1);
+            //CREATE A NEW DELETED TASK ARRAY
+            const taskDel = e.target.closest("li").textContent;
+            let tasksDel = [];
+            tasksDel.push(taskDel);
+            localStorage.setItem('tasksDel', JSON.stringify(tasksDel));
+
+
+        }
     }
     if (e.target === clearBtn) {
-        if (confirm("Are you sure?")){
-        Array.from(document.querySelectorAll("li")).forEach(element => {
-            element.remove();
-        });   
+        if (confirm("Are you sure?")) {
+            Array.from(document.querySelectorAll("li")).forEach(element => {
+                element.remove();
+            });
         }
+        //CLEAR TASKS CLEARS LOCAL STORAGE
+        localStorage.clear()
     }
 }
 
 //FILTER THE TASK
-function filterTasks(e){
+function filterTasks(e) {
     Array.from(document.querySelectorAll("li")).forEach(element => {
-        if (!element.innerText.includes(filter.value)){
-            element.style.display = "none";
+        if (element.innerText.toLowerCase().includes(filter.value.toLowerCase())) {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none"
         }
-    }); 
+    });
 }
+
+//SAVE THE TASKS TO STORAGE
+form.addEventListener('submit', function (e) {
+    const task = taskList.textContent;
+
+    console.log(taskList.textContent)
+    let tasks = [];
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    alert('Task saved');
+
+    e.preventDefault();
+});
