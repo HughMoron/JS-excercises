99e413d8eab34600e6a79e83ad0f445dc3d59916
mendritzk.io <hi@mendritzk.io>
2020-01-06 12:13:01 +0100

    Favourite dish

--- Class/2020/CW1/app.js
+++ Class/2020/CW1/app.js
@@ -2,6 +2,66 @@
 
 document.body.style.color = "grey";
 
-let strt = (name) => `Have a good new year 2020 ${name}!`;
+let strt = (name) => `Have a good new year 2020 ${name}!<br>`;
 
 document.write(strt("peter"));
+
+//BIRTHDAY CHALLENGE
+let brthdy = (yr = new Date().getFullYear(), brth = 1990) => `The age is ${yr-brth}.<br>`;
+
+document.write(brthdy());
+
+//PETNAME CHALLENGE
+let person1 = {
+    name: "Ali",
+    age: 10,
+    gender: "d",
+    countryOfOrigin: "Brazil",
+}
+
+let petName = (person) => (person.age < 15) ? `Hi A.<br>` : `Hi B.<br>`;
+
+document.write(petName(person1));
+
+//FAVOURITE DISH
+guest1 = {name: "Ali", favDish:"kebab"};
+guest2 = {name: "Bli", favDish:"pizza"};
+guest3 = {name: "Cli", favDish:"pasta"};
+
+
+let guests = [guest1,guest2,guest3];
+let dishes =  ["hamburger","pizza","pasta"]
+
+let happy = (arrOfDishes,arrOfGuests)=>{
+    let noLuck = [];
+for(i=0; i<arrOfGuests.length; i++){
+    for(j=0; j<arrOfDishes.length; j++){
+        (arrOfGuests[i].favDish===arrOfDishes[j])?
+        document.write(`${arrOfGuests[i].name}, we do have your Meal.<br>`):noLuck.push(arrOfGuests[i]);
+          
+        (noLuck.filter(x => x == arrOfGuests[i]).length>2)?document.write( `${arrOfGuests[i].name}, we do NOT haveyour Meal.<br>`):"";      
+}
+}
+console.log(noLuck)
+}
+document.write(happy(dishes,guests));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
