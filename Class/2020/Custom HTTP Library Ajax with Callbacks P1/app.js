23f216ecba668a11d71b68a660f6929ea0a6c71f
mendritzk.io <hi@mendritzk.io>
2020-01-29 09:21:25 +0100

    easyHTTP

--- Class/2020/Custom HTTP Library Ajax with Callbacks P1/app.js
+++ Class/2020/Custom HTTP Library Ajax with Callbacks P1/app.js
@@ -1,5 +1,10 @@
 const http = new easyHTTP;
-http.get("https://jsonplaceholder.typicode.com/posts",function(items){
-    items.error?document.write(items.message): document.write(items.success)
-    console.log(items)
+
+// Get Posts
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
 });
