faf3ee9c856f8a1292a9fe1f5482de5863a149cc
mendritzk.io <hi@mendritzk.io>
2020-02-04 14:38:18 +0100

    done

---
+++ Class/2020/New Features JS2018 Iterators and Generators/DatingApp MiniProject/app.js
@@ -0,0 +1,66 @@
+const data = [{
+        name: "John Doe",
+        age: 32,
+        gender: "male",
+        lookingFor: "female",
+        location: "HalloLolo",
+        image: `https://randomuser.me/api/portraits/men/71.jpg`
+    },
+    {
+        name: "Jen Smith",
+        age: 26,
+        gender: "female",
+        lookingFor: "male",
+        location: "HelloLolo",
+        image: `https://randomuser.me/api/portraits/women/82.jpg`
+    },
+    {
+        name: "William Johnson",
+        age: 30,
+        gender: "male",
+        lookingFor: "female",
+        location: "HiLolo",
+        image: `https://randomuser.me/api/portraits/men/70.jpg`
+    }
+]
+
+const profiles = profileIterator(data)
+//Call the first profile
+nextProfile()
+
+//Next Event
+document.getElementById("next").addEventListener(`click`, nextProfile)
+
+// Event Handler: Display the next Profile
+function nextProfile() {
+    const currentProfile = profiles.next().value;
+    if (currentProfile !== undefined) {
+        document.getElementById(`profileDisplay`).innerHTML = `<ul class="list-group">
+    <li class="list-group-item">Name: ${currentProfile.name}</li>
+<li class="list-group-item">Age: ${currentProfile.age}</li>
+<li class="list-group-item">Location: ${currentProfile.location}</li>
+<li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingFor}</li>
+</ul>`
+
+        document.getElementById(`imageDisplay`).innerHTML = `<img src= "${currentProfile.image}">`
+    } else {
+        //No more profiles
+        location.reload()
+    }
+}
+
+//Profile Iterator
+function profileIterator(profiles) {
+    let nextIndex = 0;
+
+    return {
+        next: function () {
+            return nextIndex < profiles.length ? {
+                value: profiles[nextIndex++],
+                done: false
+            } : {
+                done: true
+            }
+        }
+    }
+}
---
+++ Class/2020/New Features JS2018 Iterators and Generators/Destructuring/app.js
@@ -0,0 +1,5 @@
+// Destructuring Assignment
+let a,b,c;
+[a,b,c] = [100,200,300]
+
+document.write(a,b,c)
