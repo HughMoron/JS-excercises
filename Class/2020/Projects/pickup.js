5a9a528f8a2278487c62d229947ac21722fb5640
mendritzk.io <hi@mendritzk.io>
2020-02-11 14:02:08 +0100

    DCI Invaders - Now the game :(

--- Class/2020/Projects/pickup.js
+++ Class/2020/Projects/pickup.js
@@ -4,24 +4,29 @@ document.querySelector(".enter").addEventListener("click", whoWillYouChoose)
 function whoWillYouChoose(e) {
     e.target.remove();
 
-    document.querySelector("#playerPick").style.display = "flex";
+    document.querySelector("#playerPick").style.display = "block";
     e.preventDefault()
 }
-
-//  PLAYER PICK
-// document.getElementById("playerPick").addEventListener("click",letsGo)
 
 function letsGo(e) {
     // Display change    
     document.getElementById("playerPick").remove()
     document.getElementById("game").style.display = "block";
 
-    // Music start
+    // MUSIC START
     let audioplayer = document.getElementById("play")
     audioplayer.classList = "playerButton fas fa-pause";
-        audioplayer.id = "stop"
-        song.autoplay = true
-        song.load()
+        audioplayer.id = "stop";
+        song.autoplay = true;
+        song.load();
+    audioplayer.parentElement.classList = "playerAnim"
+    console.log(audioplayer.parentElement)
+
+    //LET'S GO!!        
+    let go =document.createElement("p")
+    go.id= "go"
+    go.innerHTML = `OK GO!`
+    document.querySelector(".containerEnter").appendChild(go)
 
     // PLAYERS
     // Saf-1 
