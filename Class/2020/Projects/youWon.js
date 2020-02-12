e2797361d190be2d118edd2ee9331328c93abe5f
mendritzk.io <hi@mendritzk.io>
2020-02-12 23:50:27 +0100

    Done as good as possible

--- Class/2020/Projects/youWon.js
+++ Class/2020/Projects/youWon.js
@@ -1,3 +1,9 @@
+document.querySelector(".highscore").addEventListener("click", highscore)
+
+document.getElementById("submit").addEventListener("click", storeLocal)
+
+document.querySelector(".restart").addEventListener("click", ()=> location.reload())
+
 function youWon() {
     if (score == 20) {
         document.getElementById("game").remove();
@@ -10,20 +16,54 @@ function youWon() {
     }
 }
 
-document.querySelector(".highscore").addEventListener("click", highscore)
 
 function highscore() {
     document.getElementById("youWon").remove()
     let legend = document.getElementById("legend")
     legend.style.display = "flex";
-    
+
     event.preventDefault()
     let div = document.createElement("div");
     div.id = "score";
     let tn = document.createTextNode(`Your Score was: ${score} Points! Enter your Name to become a DCI Legend!`)
     div.appendChild(tn)
-    console.log(legend.childNodes)
-    legend.insertBefore(div,legend.childNodes[1])
-    
+    // console.log(legend.childNodes)
+    legend.insertBefore(div, legend.childNodes[1])
+
 }
+
+function storeLocal() {
+    // localStorage.clear()
     
+    const legend = {
+        name: document.getElementById("legendName").value,
+        score: score}
+    let highscores;
+    
+    if (localStorage.getItem("highscores") === null) {
+        highscores = [];
+    } else {
+        highscores = JSON.parse(localStorage.getItem("highscores"))
+    }
+    
+    highscores.push(legend)
+    highscores.sort((a,b) => b.score - a.score)
+    highscores.splice(3)
+    
+    
+    localStorage.setItem("highscores",JSON.stringify(highscores))
+    
+    const highScoreList = document.querySelector(".highScoreList")
+    
+    highScoreList.innerHTML = 
+    highscores.map(leg =>{
+      return  `<li>${leg.name}: ${leg.score}</li>`
+    }).join("");
+    
+    console.log(highScoreList)
+    console.log(highscores)
+    
+    document.getElementById("legend").remove()
+    document.getElementById("highscores").style.display = "flex"
+    event.preventDefault()
+}
