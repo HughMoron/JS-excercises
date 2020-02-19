af7ec41f13de9f3b4ffc1407edefd32730a5a0b5
mendritzk.io <hi@mendritzk.io>
2020-02-19 11:37:15 +0100

    Webpack started

---
+++ Class/2020/Class/4_JavaScript/08-Modules/functional-components/main.js
@@ -0,0 +1,7 @@
+// import {greeting,hi} from "./user.js"
+
+// console.log(hi("Eren"))
+
+import {sqr} from "./user.js"
+
+console.log(sqr(2))
---
+++ Class/2020/Class/4_JavaScript/08-Modules/functional-components/user.js
@@ -0,0 +1,12 @@
+//add export before declaration
+// let greeting = "welcome back"
+
+// function hi(user){
+//     return `${greeting} ${user} in Javascript Module.`
+// }
+
+// // add export after declaration
+// export{greeting,hi}
+
+// add export before declaration
+export function sqr(num) {return num*num}
