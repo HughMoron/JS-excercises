d98c47850dac637036903a1d2b8c462bd49f6b3a
mendritzk.io <hi@mendritzk.io>
2020-02-26 11:15:49 +0100

    React started

---
+++ Class/2020/Excercises/React/200225 Good Morning/my-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Header from './components/header'
+
+
+class App extends Component{
+  render(){
+    return (
+   
+      <div className="App-header">
+       <Header />
+      </div>
+    
+  );
+  }
+}
+
+export default App;
---
+++ Class/2020/Excercises/React/200225 Good Morning/my-app/src/components/header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './Header.css'
+
+class Header extends Component{
+    render(){
+        return(
+            <header className = "Header">
+                <a href='#'>Home Page</a>
+            </header>
+        )
+    }
+
+}
+
+
+export default Header;
