f4a944b3e173faf5f6f74775eb27566a972f7ec7
mendritzk.io <hi@mendritzk.io>
2020-03-12 09:42:42 +0100

    Update of usersearch through LiveCode

--- Class/2020/Class/6_React/0.usersearch/4/src/App.jsx
+++ Class/2020/Class/6_React/0.usersearch/4/src/App.jsx
@@ -3,13 +3,19 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar.jsx";
 import axios from "axios";
 import Users from "./components/users/Users.jsx";
+import Search from "./Search";
 
 class App extends Component {
   state = {
     users: []
   };
   async componentDidMount() {
-    const res = await axios.get("https://api.github.com/users");
+    const APIKey = process.env.REACT_APP_CLIENT_ID;
+    const APISecret = process.env.REACT_APP_CLIENT_SECRET;
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${APIKey}&client_secret=${APISecret}`
+    );
     this.setState({ users: res.data });
   }
 
@@ -17,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <Navbar />
+        <Search />
         <Users users={this.state.users} />
       </div>
     );
--- Class/2020/Class/6_React/0.usersearch/4/src/Search.jsx
+++ Class/2020/Class/6_React/0.usersearch/4/src/Search.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
 class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
   render() {
-    return <div></div>;
+    return;
+    <form className='form'>
+      <input
+        type='text'
+        name='text'
+        value={this.state.text}
+        placeholder='Search Users...'
+        onChange={this.changeHandler}
+        //   => if we have a class component in React
+      />
+
+      <input type='submit' className='btn btn-dark btn-block' value='Search' />
+    </form>;
   }
 }
 
