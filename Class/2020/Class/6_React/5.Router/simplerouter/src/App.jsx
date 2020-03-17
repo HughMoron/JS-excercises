912cf7c3e985fa76346ab18bf0f7542ab7306988
hughmoron <hi@mendritzk.io>
2020-03-17 14:50:29 +0100

    router

---
+++ Class/2020/Class/6_React/5.Router/simplerouter/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import './App.css';
+
+const Home = () => <h1>Home</h1>
+const About = () => <h1>About</h1>
+const App = () => {
+  return (
+<Router>
+  <Fragment>
+     
+  </Fragment>
+</Router>
+  );
+}
+
+export default App;
