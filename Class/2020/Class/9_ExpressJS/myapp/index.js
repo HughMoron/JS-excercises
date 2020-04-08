0a2a580f391a7e217fe1740a06b9f2825de8796f
hughmoron <hi@mendritzk.io>
2020-04-08 12:23:46 +0200

    slow recovery approached

--- Class/2020/Class/9_ExpressJS/myapp/index.js
+++ Class/2020/Class/9_ExpressJS/myapp/index.js
@@ -1,19 +1,20 @@
+require("dotenv").config();
 const express =  require('express');
-const morgan = require('morgan')
-
+const morgan = require('morgan');
 const app = express()
 
-const PORT = 8585;
+const PORT = process.env.PORT || 8585;
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname));
+// app.use(morgan("dev"));
+// app.use(express.static(__dirname));
 
-app.get('/',(req,res)=>{
-    res.send("Hello there...")
-})
+// app.get('/',(req,res)=>{
+//     res.send("Hello there...")
+// })
 
-app.listen(PORT,() => {
-    console.log(`Application is listening to the port: ${PORT}`)
-})
+// app.listen(PORT,() => {
+//     console.log(`Application is listening to the port: ${PORT}`)
+// })
 
-Array.filter()
+// Array.filter()
+console.log(process.env.PORT)
