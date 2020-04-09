4d0813c32c533882db17f09aae8917ea3605df82
hughmoron <hi@mendritzk.io>
2020-04-09 16:44:07 +0200

    day by day

--- Class/2020/Class/9_ExpressJS/myapp/index.js
+++ Class/2020/Class/9_ExpressJS/myapp/index.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const path = require("path")
-const members = require('./Members')
-const app = express()
+const path = require("path");
+const logger = require("./middleware/logger")
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-const logger = (req,res,next) => {
-    console.log(`${req.protocol}://${req.get('host')}${res.originalURL}`);
-    next();
-}
 
 // Init Middleware
-app.use(logger);
-
-// Get All Members
-app.get('/api/members', (req, res) => res.json(members));
+// app.use(logger);
 
 // Set static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Members Api Routes
+app.use('/api/members', require('./routes/api/members'))
+
+
 
 app.listen(PORT, () =>
     console.log(`Server started on port ${PORT}.`))
+
+console.log()
